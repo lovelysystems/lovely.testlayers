4ee2861582d2a2551555fbe3c68d86353bdccf86
dobe <bernddorn@gmail.com>
2010-10-22 06:15:56 +0000

    version bump

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ open('doc.txt', 'w').write(long_description)
 
 setup(
     name = 'lovely.testlayers',
-    version = '0.1.0',
+    version = '0.1.1',
     description="Testing Layers for use with zope.testing",
     long_description=long_description,
     packages = find_packages('src'),
