ea53129bee810a903718ba5db069001c1154569f
Mathias Fussenegger <f.mathias@zignar.net>
2013-09-06 08:54:45 +0200

    prepare release 0.6.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ long_description='\n'.join((
 
 setup(
     name = 'lovely.testlayers',
-    version = '0.5.3',
+    version = '0.6.0',
     description="mysql, postgres nginx, memcached cassandra test"+\
                 " layers for use with zope.testrunner",
     long_description=long_description,
