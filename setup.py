5061186268379d3f6d02be2bf1ca04f025f1f2c3
Bernd Rössl <bernd.roessl@gmail.com>
2015-03-12 12:02:39 +0100

    release 0.6.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ long_description='\n'.join((
 
 setup(
     name = 'lovely.testlayers',
-    version = '0.6.0',
+    version = '0.6.1',
     description="mysql, postgres nginx, memcached cassandra test"+\
                 " layers for use with zope.testrunner",
     long_description=long_description,
